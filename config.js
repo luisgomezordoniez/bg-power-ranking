1fc4195975c63c2df6f3a42770d1d8dc09950488
Luis <luis.gomez.ordoniez@gmail.com>
2026-04-17 05:30:49 -0400

    config file

---
+++ config.js
@@ -0,0 +1,5 @@
+// ============================================================
+// TABLETOP POWER RANKINGS — Configuration
+// Update API_URL here when you redeploy Apps Script
+// ============================================================
+const API_URL = "https://script.google.com/macros/s/AKfycbwn106lQFmDgLnFJh0cuzzuf4s1TKIJLvU1qTrhTGz1u5_Er_IcnpFc-it_k9Is4s2b/exec";
